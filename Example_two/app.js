44e01023903084ad59bbb03013bbafb44fd42034
Ana Castellanos <ana.lec15@gmail.com>
2018-03-14 15:58:26 -0600

    Empezando de cero

--- Example_two/app.js
+++ Example_two/app.js
@@ -1,32 +1,55 @@
-var mongoose = require('mongoose'),
-	express = require("express"),
+var express = require("express"),
+	app = express(),
 	bodyParser = require("body-parser"),
-	app = express();
+	methodOverride = require("method-override"),
+	mongoose = require('mongoose');
 
-var Models = require("./models/tvshow");
-
-
-//Middlewares
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json() );
+app.use(bodyParser.json());
+app.use(methodOverride());
 
-//Importar models
-
-
-//Rutas
 var router = express.Router();
 
-router.get( '/', function(require, response){
-	response.send("Hola Mundo!");
+router.get('/', function(require, response) {
+	response.send("Hola Mundo");
 });
 
 app.use(router);
 
+app.listen(3000, function(){
+	console.log("Node server runing on http://localhost:3000");
+});
 
-//Iniciar servidor
-app.listen(3000, function() {
-		console.log("Node server running on http://localhost:3000");
-});	
+
+
+
+
+// var mongoose = require('mongoose'),
+// 	express = require("express"),
+// 	// bodyParser = require("body-parser"),
+// 	app = express();
+
+// var Models = require("./models/tvshow");
+
+
+// //Middlewares
+// // app.use(bodyParser.urlencoded({ extended: false}));
+// // app.use(bodyParser.json() );
+
+// //Rutas
+// // var router = express.Router();
+
+// // router.get( '/', function(require, response){
+// // 	response.send("Hola Mundo!");
+// // });
+
+// // app.use(router);
+
+
+// //Iniciar servidor
+// app.listen(3000, function() {
+// 		console.log("Node server running on http://localhost:3000");
+// });	
 
 
 
