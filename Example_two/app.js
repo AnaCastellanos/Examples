594bad65505d507f735708c670b68e8f82ba2a93
Ana Castellanos <ana.lec15@gmail.com>
2018-04-18 00:01:29 -0500

    Change app.js

--- Example_two/app.js
+++ Example_two/app.js
@@ -2,15 +2,22 @@ var express = require("express"),
 	app = express(),
 	bodyParser = require("body-parser"),
 	methodOverride = require("method-override"),
-	mongoose = require('mongoose');
+
+//Conexión con MongoDB
+'use strict'
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/auth'); 
+//El método connetc() se conecta a una base de datos llamada auth, se crea automaticamente.
+module.exports = mongoose;
+
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 app.use(methodOverride());
-
+ana
 var router = express.Router();
 
-router.get('/', function(require, response) {
+router.get('/', function(request, response) {
 	response.send("Hola Mundo");
 });
 
